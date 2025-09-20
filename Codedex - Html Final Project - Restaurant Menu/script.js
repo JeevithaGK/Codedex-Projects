2980d9229e4cc08655f12dc3b77e3061cfc3e8a4
JeevithaGK <jeevitha26091@gmail.com>
2025-09-20 20:35:44 +0530

    Add files via upload

---
+++ Codedex - Html Final Project - Restaurant Menu/script.js
@@ -0,0 +1,30 @@
+const a = "august";
+if (a==="january"){
+    console.log("♑ Capricorn (January): Hard work pays off. Stay focused on your long-term goals.")
+} else if(a==="february"){
+    console.log("♒ Aquarius (February): Innovation is your strength. Trust your unique ideas and creativity.")
+}else if(a==="march"){
+    console.log("♓ Pisces (March): Emotions guide you now. Listen to your intuition for clarity.")
+}else if(a==="april"){
+    console.log("♈ Aries (April): Bold actions bring results. Lead with confidence and courage.")
+}else if(a==="may"){
+    console.log("♉ Taurus (May): Stability surrounds you. Enjoy comfort and stay grounded.")
+}else if(a==="june"){
+    console.log("♊ Gemini (June): Communication flows easily. Great time to share and connect.")
+}else if(a==="july"){
+    console.log("♋ Cancer (July): Nurture your inner world. Home and heart take center stage.")
+}else if(a==="august"){
+    console.log("♌ Leo (August): You’re in the spotlight. Let your confidence and charm shine.")
+}else if(a==="september"){
+    console.log("♍ Virgo (September): Focus on details. Small efforts now lead to big rewards.")
+}else if(a==="october"){
+    console.log("♎ Libra (October): Balance is key. Make decisions with harmony in mind.")
+}else if(a==="november"){
+    console.log("♏ Scorpio (November): Change is coming. Embrace transformation with strength.")
+}else if (a==="december") {
+    console.log("♐ Sagittarius (December): Adventure awaits. Explore, learn, and grow freely.")
+} else {
+    console.log("Invalid month")
+}
+
+console.log(a);
