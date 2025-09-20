11700043985b805ce2610d7aef64dc1acb91372a
JeevithaGK <jeevitha26091@gmail.com>
2025-09-20 20:36:37 +0530

    Add files via upload

---
+++ Codedex - Javascript Final Project - View the weather/script.js
@@ -0,0 +1,31 @@
+const apiKey = '9988c05708177e58dcedf3677c1f07cb'; 
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", function () {
+  const city = document.getElementById("cityInput").value.trim();
+  if (city) {
+    getWeather(city);
+  }
+});
+
+function getWeather(city) {
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}&units=metric`;
+  console.log("Fetching from:", apiURL);
+
+  fetch(apiURL)
+    .then(response => response.json())
+    .then(data => {
+      console.log("API response:", data);
+      if (data.cod === 200) {
+        document.getElementById("cityName").innerText = `City: ${data.name}`;
+        document.getElementById("temp").innerText = `Temperature: ${data.main.temp}°C`;
+        document.getElementById("description").innerText = `Weather: ${data.weather[0].description}`;
+        document.getElementById("weatherResult").classList.remove("hidden");
+      } else {
+        alert(`Error: ${data.message}`);
+      }
+    })
+    .catch(error => {
+      console.error("Fetch error:", error);
+    });
+}
